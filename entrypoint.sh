b9815b2c8eb01a9771113540b29b8cbb5feecf1e
jainil-incquet <jainil.p@incquet.com>
2026-05-28 17:59:11 +0530

    fix: update IP detection URL in entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 # entrypoint.sh
 
 echo "🔹 Detecting Public IP..."
-export ANNOUNCED_IP=$(curl -s https://ipify.org)
+export ANNOUNCED_IP=$(curl -s https://api.ipify.org)
 echo "✅ Public IP detected: $ANNOUNCED_IP"
 
 # Start the application
